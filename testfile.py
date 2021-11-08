cff560c5423e23bc537ebe5e07f73ae299ed6491
vaishaliceles <84778498+vaishaliceles@users.noreply.github.com>
2021-11-08 16:44:18 +0530

    Create testfile.py

---
+++ testfile.py
@@ -0,0 +1,8 @@
+import random
+
+
+print(f"Printing a randome number{random.random()}")
+print("Lets test")
+print("one more user added")
+print("fourth user")
+print("to check amount of credit debited")
